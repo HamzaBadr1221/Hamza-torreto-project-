f86bdd49a347a69cb89900e90ad17b6c2e86e1cc
marksherif105 <domanictoretto95@gmail.com>
2026-09-19 21:03:22 +0300

    Last Update

--- lib/features/products/presentation/pages/product_details_page.dart
+++ lib/features/products/presentation/pages/product_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import '../cubit/product_cubit.dart';
 import '../cubit/product_state.dart';
@@ -24,7 +25,15 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Product Details')),
+      appBar: AppBar(
+        title: const Text('Product Details'),
+        leading: IconButton(
+          onPressed: () {
+            context.push('/products');
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
       body: BlocBuilder<ProductCubit, ProductState>(
         builder: (context, state) {
           return state.when(
